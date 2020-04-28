edb46584ab66d0a1df90d68c0632b57c56ec758c
Gbemileke AJIBOYE <gbemileke@seamlesshr.com>
2020-04-28 12:03:33 +0100

    Integrate login and register services

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -40,7 +40,7 @@ const logout = (state, action) => {
 };
 
 const registerFail = (state, action) => {
-    return updateObject(state, { errors: action.errors});
+    return updateObject(state, { errors: action.errors, loading: true});
 };
 
 const reducer = ( state = initialState, action ) => {
